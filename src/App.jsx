b27c287b662d8fd55e66899e2736de85c8a538bd
usuny0317 <usuny0317@naver.com>
2025-01-27 12:26:09 +0900

    체크박스 정렬까지 수행

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -10,6 +10,16 @@ function App() {
 
   const [countrys, setCountrys] =useState([])
 
+  const [ischeck, setIscheck] = useState(false)
+
+  useEffect(()=>{
+    //눌렸을 때 ischeck은 트루
+    //안눌렸을 때 ischeck은 false
+    const sortnow=sortmadel([...countrys])
+    setCountrys(sortnow)
+
+  },[ischeck])
+
   function reset(){
     setName("")
     setGold(0)
@@ -17,35 +27,55 @@ function App() {
     setBronze(0)
   }
 
+  function sortmadel(before){
+    if(ischeck){
+      return before.sort((a,b)=>{
+        const totala= a.gold+a.silver+a.bronze;
+        const totalb=b.gold+b.silver+b.bronze;
+        return totalb-totala
+      })
+    }else{return before.sort((a,b)=>b.gold-a.gold)}
+  }
+
 
   function addCountry(e){
     e.preventDefault();
-    const newCountry={
-      name: name,
-      gold: gold,
-      silver: silver,
-      bronze: bronze
-    }
-    const nowCountry=[...countrys, newCountry]
-    setCountrys(nowCountry)
-    reset
+    const has=countrys.findIndex((item)=>item.name ===name)
+    if (has===-1){
+      const newCountry={
+        name: name,
+        gold: gold,
+        silver: silver,
+        bronze: bronze
+      }
+      const nowCountry=[...countrys, newCountry]
+      const sortnow=sortmadel(nowCountry)
+      setCountrys(sortnow)
+    }else{alert("이미 존재하는 나라입니다.")}
+    reset()
   }
 
   function updateCountry(){
-    const newcountrys= countrys.filter((country)=>country.name!==name)
-    const updatecountry={
-      name: name,
-      gold: gold,
-      silver: silver,
-      bronze: bronze
+    const has= countrys.findIndex((item)=>item.name===name)
+    if(has===-1){
+      alert("등록되지 않은 국가입니다.")
+    }else{
+      const newcountrys= countrys.filter((country)=>country.name!==name)
+      const updatecountry={
+        name: name,
+        gold: gold,
+        silver: silver,
+        bronze: bronze
+      }
+      const nowcountrys= [...newcountrys, updatecountry]
+      const sortnow=sortmadel(nowcountrys)
+
+      setCountrys(sortnow)
     }
-    const nowcountrys= [...newcountrys, updatecountry]
-    setCountrys(nowcountrys)
-    reset
+    reset()
   }
 
   function delCountry(name){
-    alert("삭제하기")
     const nowcountrys = countrys.filter((country)=>{return country.name!==name})
     setCountrys([...nowcountrys])
   }
@@ -55,6 +85,10 @@ function App() {
     <>
       <div>
         <h1>올림픽</h1>
+        <label>총 메달 수로 정렬<input type='checkbox' checked={ischeck}
+          onChange={e=>setIscheck(e.target.checked)}
+        /></label>
+        
       </div>
       <div>
         <form onSubmit={addCountry}>
@@ -64,15 +98,15 @@ function App() {
           </label>
           <label>
             금메달
-            <input value={gold} onChange={(e)=>{setGold(e.target.value)}} min={0}/>
+            <input value={gold} onChange={(e)=>{setGold(+e.target.value)}} min={0}/>
           </label>
           <label>
             은메달
-            <input value={silver} onChange={(e)=>{setSilver(e.target.value)}} min={0}/>
+            <input value={silver} onChange={(e)=>{setSilver(+e.target.value)}} min={0}/>
           </label>
           <label>
             동메달
-            <input value={bronze} onChange={(e)=>{setBronze(e.target.value)}} min={0}/>
+            <input value={bronze} onChange={(e)=>{setBronze(+e.target.value)}} min={0}/>
           </label>
           <label>
             <input type='submit'value={"추가하기"}/>
